dc9c8cbc99502aed08ea809ddd1adc99122f9a23
fanjl <348196333@qq.com>
2019-10-10 16:02:00 +0800

    spring-cloud-example

---
+++ spring-cloud-feign/src/main/java/com/fjl/springcloudfeign/SpringCloudFeignApplication.java
@@ -0,0 +1,18 @@
+package com.fjl.springcloudfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class SpringCloudFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFeignApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-hystrix/src/main/java/com/fjl/springcloudhystrix/SpringCloudHystrixApplication.java
@@ -0,0 +1,29 @@
+package com.fjl.springcloudhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class SpringCloudHystrixApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHystrixApplication.class, args);
+    }
+
+}
